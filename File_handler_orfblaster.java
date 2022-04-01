5ff9fd219b4ede910479b67d8c38cf6ee730b3a1
stefvanbreemen <SJT.vanbreemen@student.han.nl>
2022-04-01 18:59:13 +0200

    blast werkt!

--- File_handler_orfblaster.java
+++ File_handler_orfblaster.java
@@ -13,32 +13,56 @@ import java.util.*;
 public class File_handler_orfblaster {
 
     File directoryPath_BLAST = new File("test.tsv");
-    HashMap<String, String> resultMap_ORF;
+    LinkedHashMap<String, String> resultMap_ORF;
     ArrayList<String[]> resultMap_BLAST;
 
 
-    
     /** 
      * @param path
      * @throws FileNotFoundException
      */
     public void read_ORF_File(String path) throws FileNotFoundException {
         File directoryPath_ORF = new File(path);
-        try (Scanner scan_ORF = new Scanner(directoryPath_ORF)) {
-            resultMap_ORF = new HashMap<>();
-            while (scan_ORF.hasNextLine()) {
-                String[] keys = scan_ORF.nextLine().split("\n");
-                String[] values = scan_ORF.nextLine().split("\n");
-                for (int i = 0; i < keys.length; i++) {
-                    String Sequence = values[i];
-                    System.out.println(Sequence);
-                    System.out.println("\n");
-                    resultMap_ORF.put(keys[i], values[i]);
+        resultMap_ORF = new LinkedHashMap<String, String>();
+        try {
+                    Scanner scanner = new Scanner(directoryPath_ORF);
+                    String header = "";
+                    String sequence = "";
+                    while (scanner.hasNextLine()) {
+                        String line = scanner.nextLine();
+                        if (line.startsWith(">")) {
+                            if (!header.equals("")) {
+                                System.out.println(header);
+                                System.out.println(sequence);;
+                                resultMap_ORF.put(header, sequence);
+                            }
+                            header = line.substring(1);
+                            sequence = "";
+                        } else {
+                            sequence += line;
+                        }
+                    }
+                    resultMap_ORF.put(header, sequence);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
                 }
-            }
-        }
-    }
 
+        // try (Scanner scan_ORF = new Scanner(directoryPath_ORF)) {
+        //     resultMap_ORF = new LinkedHashMap<>();
+        //     while (scan_ORF.hasNextLine()) {
+        //         String[] keys = scan_ORF.nextLine().split("\n");
+        //         String[] values = scan_ORF.nextLine().split("\n");
+        //         for (int i = 0; i < keys.length; i++) {
+        //             System.out.println(keys[i]);
+        //             String Sequence = values[i];
+        //             System.out.print(Sequence);
+        //             System.out.print("\n");
+        //             resultMap_ORF.put(keys[i], values[i]);
+        //         }
+        //     }
+        // }
+    }
+    
 
     // public static void read_BLAST_File() throws IOException {
     //     try (Scanner scan_BLAST = new Scanner(directoryPath_BLAST)) {
